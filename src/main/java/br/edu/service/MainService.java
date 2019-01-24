02bdf7525e43a30e7b7e960c983caba1a21d9fc2
fmachado <fmachado@scopus.com.br>
2019-01-24 17:35:48 -0200

    Sprint-2

--- src/main/java/br/edu/service/MainService.java
+++ src/main/java/br/edu/service/MainService.java
@@ -1,26 +1,20 @@
 package br.edu.service;
 
-import br.edu.client.RemoteClient;
 import br.edu.domain.ResultadoValidacao;
 
 public class MainService {
 	
-	private RemoteClient remoteClient;
-	
-	public MainService(RemoteClient remoteClient) {
-		this.remoteClient = remoteClient;
+	public MainService() {
 	}
 
 	public ResultadoValidacao calculo (Integer fator, Double valor) {
 		ResultadoValidacao result = null;
 		double resultadoCalculo;
 		
-		Integer indice = getIndice(fator);
-		
 		if (fator == 0) {
-			resultadoCalculo = formulaSemFator(valor, indice);
+			resultadoCalculo = formulaSemFator(valor);
 		} else {
-			resultadoCalculo = formulaFator(fator, valor, indice);
+			resultadoCalculo = formulaFator(fator, valor);
 		}
 				
 		
@@ -39,35 +33,13 @@ public class MainService {
 		
 	}
 
-	private double formulaSemFator(Double valor, Integer indice) {
-		if (indice > 0) {
-			return valor / indice;
-		} else {
-			return valor;
-		}
+	private double formulaSemFator(Double valor) {
+		return valor / 3;
 		
 	}
 
-	private double formulaFator(Integer fator, Double valor, Integer indice) {
-		if (indice > 0) {
-			return (fator * valor) / indice;
-		} else {
-			return (fator * valor);
-		}
-	}
-	
-	private Integer getIndice(Integer fator) {
-		Integer indice = remoteClient.getRemoteIndice();
-		
-		if (indice < 0) {
-			if (fator == 0) {
-				indice = 3;
-			} else {
-				indice = 2;
-			}
-		}
-		
-		return indice;
+	private double formulaFator(Integer fator, Double valor) {
+		return (fator * valor) / 2;
 	}
 	
 }

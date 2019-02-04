eada0da7db864a2797f5dd683e3248356c8438ab
fmachado <fmachado@scopus.com.br>
2019-02-04 17:14:01 -0200

    finish

--- src/main/java/br/edu/service/MainService.java
+++ src/main/java/br/edu/service/MainService.java
@@ -7,7 +7,40 @@ public class MainService {
 	public MainService() {}
 
 	public ResultadoValidacao calculo (Integer fator, Double valor) {
-		return  null;
+		double result;
+
+		result = calculoPrivado(fator, valor);
+		
+		ResultadoValidacao resultado;
+		
+		
+		resultado = classificacao(result);
+		
+		return resultado;
+	}
+
+	private ResultadoValidacao classificacao(double result) {
+		ResultadoValidacao resultado;
+		if (result < 0) {
+			resultado = ResultadoValidacao.INVALIDO;												 
+		} else if (result <= 1000) {
+			resultado = ResultadoValidacao.VALIDO;
+		} else if (result <=5000) {
+			resultado = ResultadoValidacao.EXTRA;
+		} else {
+			resultado = ResultadoValidacao.BONUS;
+		}
+		return resultado;
+	}
+
+	private double calculoPrivado(Integer fator, Double valor) {
+		double result;
+		if (fator == 0) {
+			result = valor / 3;
+		} else {
+			result = (fator * valor) / 2;	
+		}
+		return result;
 	}
 
 	
--- src/test/java/br/edu/service/MainServiceTest.java
+++ src/test/java/br/edu/service/MainServiceTest.java
@@ -1,6 +1,11 @@
 package br.edu.service;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import br.edu.domain.ResultadoValidacao;
 
 public class MainServiceTest {
 	
@@ -11,4 +16,104 @@ public class MainServiceTest {
 		service = new MainService();
 	}
 	
+	@Test
+	public void shouldReturnInvalidIfNegative() {
+		Integer fator = -1;
+		Double valor = 100.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.INVALIDO, actual);
+	}
+	
+	@Test
+	public void shouldReturnValidIfUpToOneThousand() {
+		Integer fator = 1;
+		Double valor = 100.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.VALIDO, actual);
+	}
+	
+	@Test
+	public void shouldReturnValidIfIsZero() {
+		Integer fator = 0;
+		Double valor = 100.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.VALIDO, actual);
+	}
+
+	@Test
+	public void shouldReturnValidIfIsThousand() {
+		Integer fator = 20;
+		Double valor = 100.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.VALIDO, actual);
+	}
+	
+	@Test
+	public void shouldReturnExtraIfFatorIs30AndValor100() {
+		Integer fator = 30;
+		Double valor = 100.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.EXTRA, actual);
+	}
+	
+	@Test
+	public void shouldReturnBonusIfGreaterThan5000() {
+		Integer fator = 230;
+		Double valor = 100.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.BONUS, actual);
+	}
+	
+	@Test
+	public void shouldReturnExtraIfFactorIs1AndValorIs2002() {
+		Integer fator = 1;
+		Double valor = 2002.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.EXTRA, actual);
+	}
+	
+	@Test
+	public void shouldReturnExtraIfFactorIs1AndValorIs10000() {
+		Integer fator = 1;
+		Double valor = 10000.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.EXTRA, actual);
+	}
+	
+	@Test
+	public void shouldReturnBonusIfFactorIs1AndValorIs10002() {
+		Integer fator = 1;
+		Double valor = 10002.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.BONUS, actual);
+	}
+
+	@Test
+	public void shouldDivideBy3IfFatorIs0() {
+		Integer fator = 0;
+		Double valor = 300000.0;
+
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.BONUS, actual);
+	}
+
 }

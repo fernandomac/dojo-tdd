fbf16257a6a869734c8d2eb6ce3d7ec297a5ea1f
fmachado <fmachado@scopus.com.br>
2019-06-28 13:16:57 -0300

    finalizado ks

--- src/main/java/br/edu/service/MainService.java
+++ src/main/java/br/edu/service/MainService.java
@@ -1,13 +1,39 @@
 package br.edu.service;
 
+import br.edu.client.RemoteClient;
 import br.edu.domain.ResultadoValidacao;
 
 public class MainService {
 	
-	public MainService() {}
-
+	private RemoteClient client;
+	
+	public MainService(RemoteClient client) {
+		this.client = client;
+	}
+	
 	public ResultadoValidacao calculo (Integer fator, Double valor) {
-		return  null;
+		ResultadoValidacao result = null;
+		
+		int indice = client.getIndice();
+		
+		Double calculo =  (fator * valor) / indice;
+		
+		
+		if( fator == 0) {
+			calculo = valor / indice;
+		}
+		
+		if (calculo < 0) {
+			result = ResultadoValidacao.INVALIDO;
+		} else if (calculo > 0 && calculo <= 1000) {
+			result = ResultadoValidacao.VALIDO;
+		} else if (calculo >1000 && calculo <= 5000) {
+			result = ResultadoValidacao.EXTRA;
+		} else {
+			result = ResultadoValidacao.BONUS;
+		}
+		
+		return result;
 	}
 
 	
--- src/test/java/br/edu/service/MainServiceTest.java
+++ src/test/java/br/edu/service/MainServiceTest.java
@@ -1,14 +1,93 @@
 package br.edu.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import br.edu.client.RemoteClient;
+import br.edu.domain.ResultadoValidacao;
 
 public class MainServiceTest {
 	
 	private MainService service;
 	
+	@Mock
+	private RemoteClient client;
+	
 	@Before
 	public void setUp() {
-		service = new MainService();
+		MockitoAnnotations.initMocks(this);
+		service = new MainService(client);
 	}
+	
+	@Test
+	public void shouldBeInvalid() {
+		Integer fator = 12;
+		Double valor = -1.0;
+		
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		
+		assertEquals(ResultadoValidacao.INVALIDO, actual);
+		
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void shouldBeValid() {
+		Integer fator = 2;
+		Double valor = 1000d;
+		
+		
+		
+		when(client.getIndice()).thenThrow(new RuntimeException());
+		
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.VALIDO, actual);
+		
+		verify(client).getIndice();
+		verifyNoMoreInteractions(client);
+		
+	}
+
+	@Test
+	public void shouldBeExtra() {
+		Integer fator = 2;
+		Double valor = 1001d;
+		
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.EXTRA, actual);
+		
+	}
+	
+	@Test
+	public void shouldBeBonus() {
+		Integer fator = 2;
+		Double valor = 5001d;
+		
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.BONUS, actual);
+		
+	}
+	
+	@Test
+	public void shouldBeValidoMenor1000WithFatorZero() {
+		Integer fator = 0;
+		Double valor = 299d;
+		
+		ResultadoValidacao actual = service.calculo(fator, valor);
+		
+		assertEquals(ResultadoValidacao.VALIDO, actual);
+		
+	}
+	
 	
 }
